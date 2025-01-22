01b3fe1a17da30faef75ad8de0d701c0d1ed1cff
aiaagentapp <49463797+aiaagentapp@users.noreply.github.com>
2025-01-22 15:46:56 +0800

    Add files via upload

---
+++ SampleSDK/SampleSDK/SampleSDK.swift
@@ -0,0 +1,14 @@
+//
+//  SampleSDK.swift
+//  SampleSDK
+//
+//  Created by HEXA-Yuheng.Chook on 21/01/2025.
+//
+
+import Foundation
+import Alamofire
+
+public enum SDK: String {
+    case test1 = "test1"
+    case test2 = "test2"
+}
---
+++ SampleSDK/SampleSDKTests/SampleSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSDKTests.swift
+//  SampleSDKTests
+//
+//  Created by HEXA-Yuheng.Chook on 21/01/2025.
+//
+
+import Testing
+@testable import SampleSDK
+
+struct SampleSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
